5ebb97f48c6a5c564d349adf2567e49f29042ce3
Vinícius Melo <viniciusdesk@icloud.com>
2019-01-08 11:47:57 -0200

    Changed README, added requirements and skeleton of tests.

---
+++ tests.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.fixture
+def data():
+    return {"data:", "5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM"}
+
+
+# TODO finish tests in code below
+def output_test(data):
+    pass

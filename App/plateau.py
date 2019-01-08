4b3aed1270a01a89da3c2431409bc43435e2c610
Melo <Vinicius.Melo1@br.nestle.com>
2019-01-08 18:15:08 -0200

    Improve validation to Plateau and Rover classes, now is checking more possibilities.

--- App/plateau.py
+++ App/plateau.py
@@ -1,22 +1,48 @@
 class Plateau:
     """ Plateau class, representing the 'map' that rovers will land and explore."""
     def __init__(self, max_x: int, max_y: int):
-        self.__max_x = max_x
-        self.__max_y = max_y
+        self.max_x = max_x
+        self.max_y = max_y
         self.__min_x = 0
         self.__min_y = 0
 
     @property
+    def max_y(self):
+        return self.__max_y
+
+    @property
     def max_x(self):
         return self.__max_x
+
+    @property
+    def min_x(self):
+        return self.__min_x
+
+    @property
+    def min_y(self):
+        return self.__min_y
+
+    @max_x.setter
+    def max_x(self, max_x):
+        if isinstance(max_x, int) and max_x >= 0:
+            self.__max_x = max_x
+        else:
+            raise ValueError(f"Wrong value setter, value informed: {max_x}")
+
+    @max_y.setter
+    def max_y(self, max_y):
+        if isinstance(max_y, int) and max_y >= 0 and max_y:
+            self.__max_y = max_y
+        else:
+            raise ValueError(f"Wrong value setter, value informed: {max_y}")
 
     def __str__(self):
         return f"Max Values: ({self.__max_x},{self.__max_y}) \nMin Values: ({self.__min_x}, {self.__min_y})"
 
 
 if __name__ == '__main__':
-    pla = Plateau('STR', 5)
-    print(pla)
-    print(pla.max_x)
-    print(type(pla))
-    print(type(pla.max_x))
+    plat = Plateau(5, 5)
+    print(plat)
+    plat2 = Plateau(5, 12)
+    print(plat2)
+    print(type(plat2))
--- App/rover.py
+++ App/rover.py
@@ -1,11 +1,77 @@
+from plateau import Plateau
+
 class Rover:
     """ Rover class, representing a robotic rover sended by NASA to mars. """
-    def __init__(self, position_x: int, position_y: int, face_direction: int, plateau: Plateau):
-        self.validation(position_x, position_y, face_direction, plateau)
+    def __init__(self, position_x: int, position_y: int, face_direction: str, plateau):
+        self.plateau = plateau
         self.position_x = position_x
         self.position_y = position_y
         self.face_direction = face_direction
-        self.plateau = plateau
 
-    def validation(self, position_x, position_y, face_direction, plateau) -> bool:
-        pass
+    @property
+    def plateau(self):
+        return self.__plateau
+
+    @plateau.setter
+    def plateau(self, plateau):
+        if isinstance(plateau, Plateau):
+            self.__plateau = plateau
+        else:
+            raise ValueError(f"Wrong plateau setter, value informed: {plateau}")
+
+    @property
+    def position_x(self):
+        return self.__position_x
+
+    @position_x.setter
+    def position_x(self, position_x):
+        validation = self.position_validation_x(position_x)
+        if validation:
+            self.__position_x = position_x
+        else:
+            raise ValueError(f"Wrong value setter, value informed: {position_x}")
+
+    @property
+    def position_y(self):
+        return self.__position_y
+
+    @position_y.setter
+    def position_y(self, position_y):
+        validation = self.position_validation_y(position_y)
+        if validation:
+            self.__position_y = position_y
+        else:
+            raise ValueError(f"Wrong value setter, value informed: {position_y}")
+
+    @property
+    def face_direction(self):
+        return self.__face_direction
+
+    @face_direction.setter
+    def face_direction(self, face_direction):
+        possibles_faces = ['N', 'S', 'E', 'W']
+        if face_direction.isalpha() and face_direction.upper() in possibles_faces \
+            and isinstance(face_direction, str):
+            self.__face_direction = face_direction
+        else:
+            raise ValueError(f"Wrong value setter, value informed: {face_direction}")
+
+    def position_validation_x(self, position):
+        try:
+            if isinstance(position, int) and position >= self.__plateau.min_x and \
+                position <= self.__plateau.max_x:
+                return True
+            else:
+                return False
+        except ValueError:
+            raise ValueError(f"Wrong value setter, value informed: {position}")
+
+    def position_validation_y(self, position):
+        try:
+            if isinstance(position, int) and position >= self.__plateau.min_y and \
+                position <= self.__plateau.max_y:
+                return True
+            else:
+                return False
+        except ValueError:
+            raise ValueError(f"Wrong value setter, value informed: {position}")
